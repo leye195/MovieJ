e357d7808760c6f744ca86294f1a62d1f476eb25
이영재 <yjlee@iyeongjaeui-MacBookPro.local>
2019-11-22 00:26:20 +0900

    Updates

---
+++ precache-manifest.c16ed3cb0cc7e91b8f5c7ba99ede4011.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f47582e6290d07c9feff69cba261306c",
+    "url": "/MovieJ/index.html"
+  },
+  {
+    "revision": "f25672edc7cdb6eefbc0",
+    "url": "/MovieJ/static/css/main.d983cca2.chunk.css"
+  },
+  {
+    "revision": "0e791cda9d8b4ab3d4ff",
+    "url": "/MovieJ/static/js/2.441bf977.chunk.js"
+  },
+  {
+    "revision": "f25672edc7cdb6eefbc0",
+    "url": "/MovieJ/static/js/main.53b46239.chunk.js"
+  },
+  {
+    "revision": "39f14567b84e30a9d2aa",
+    "url": "/MovieJ/static/js/runtime-main.66447fc6.js"
+  },
+  {
+    "revision": "27b65cb40d26f78354a4ac5abf87b2be",
+    "url": "/MovieJ/static/media/powered_by_rectangle_green.27b65cb4.svg"
+  },
+  {
+    "revision": "ac194a4bb53dfbaaf6e48a69986c8a9c",
+    "url": "/MovieJ/static/media/user.ac194a4b.svg"
+  }
+]);
