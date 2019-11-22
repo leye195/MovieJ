1a9eb2c05a79bd9b4e70e32b50cc995c80d3d002
이영재 <yjlee@iyeongjaeui-MacBookPro.local>
2019-11-22 10:54:12 +0900

    checking

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 class Header extends React.Component{
     render(){
         const{to,id,lan}=this.props;
@@ -9,8 +8,8 @@ class Header extends React.Component{
             <div>
                 <h1><a href={lan}>MovieJ</a></h1>
                 <p className="language-container">
-                <a href={lan!=="en-US"?"/MovieJ"+to+id+"/en-US":"#"} ><span className="en">En</span></a>
-                <a href={lan!=="ko-KR"?"/MovieJ"+to+id+"/ko-KR":"#"}><span className="kr">Kr</span></a>
+                <a href={lan!=="en-US"?to+id+"/en-US":"#"} ><span className="en">En</span></a>
+                <a href={lan!=="ko-KR"?to+id+"/ko-KR":"#"}><span className="kr">Kr</span></a>
                 </p>
             </div>
             </header> 
