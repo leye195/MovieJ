d6657cccaf9f27e738ccd03feb922c50c95a7c55
leye195 <leye195@naver.com>
2019-11-24 14:59:10 +0900

    added react-logger and css modified

--- src/store/configure.js
+++ src/store/configure.js
@@ -1,8 +1,10 @@
-import {createStore} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from '../reducers';
+import { createLogger } from 'redux-logger';
 
 const configure=()=>{
-    const store=createStore(reducers);
+    const logger = createLogger(); 
+    const store=createStore(reducers,applyMiddleware(logger));
     return store;
 }
 export default configure;
