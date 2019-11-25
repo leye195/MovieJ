bf3f5a2e7cb49d17f4c4b328e4fc993388f19fab
leye195 <leye195@naver.com>
2019-11-25 21:26:02 +0900

    191125 css modified

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -19,10 +19,10 @@ class Movie extends React.Component{
                     <div className={view+"item_content"}>
                         <div>
                             <h2 className="item_title">{title}</h2>
+                            <h5 className="item_rate">{`${avg_rate}/10`}</h5>    
                         </div>
                         <div>
                             <p><strong>{lan==="en-US"?"Released: "+release_date:"개봉 일: "+release_date}</strong></p>
-                            <h5 className="item_rate">{`${avg_rate}/10`}</h5>    
                         </div>
                         <div className="overview_container">
                             <p className="overview">{overview===""?"해당 언어의 줄거리가 존재하지 않습니다":overview}</p>
