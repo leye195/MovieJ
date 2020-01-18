014fbc7c9058ccb89edbde57e0d25742ce311023
leye195 <leye195@naver.com>
2020-01-18 17:11:04 +0900

    dotenv and refactoring

--- server/db.js
+++ server/db.js
@@ -1,9 +1,12 @@
-module.exports=(name)=>{
-    let mongoose=require('mongoose');
-    let db=mongoose.connection;
-    db.on('error',console.error);
-    db.once('open',()=>{
-        console.log("Connected to mongodb server");
-    })
-    mongoose.connect(`mongodb://localhost/${name}`);
-}
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoose=require('mongoose');
+mongoose.connect(process.env.LOCAL_DB_URL,{
+    useNewUrlParser:true,
+    useFindAndModify:false
+});
+const db=mongoose.connection;
+db.on('error',console.error);
+db.once('open',()=>{
+    console.log("Connected to mongodb server");
+})
--- server/main.js
+++ server/main.js
@@ -4,11 +4,9 @@ const cors=require('cors');
 const app=express();
 const session=require('express-session');
 const helmet=require('helmet');
+const dotenv=require('dotenv');
 //const mongoStore=require('connect-mongo')(session);
-let mongoose=require('mongoose');
-let db=mongoose.connection;
 const fileStore=require('session-file-store')(session);
-//const db=require('./db');
 let options = {
     secret: '1c!@#$#k@s1*$%t',
     resave:false,   //계속 저장 false
@@ -18,20 +16,15 @@ let options = {
         //collection:"sessions"
     //}) 
 }
+dotenv.config();
 app.use(session(options));
 app.use(helmet());
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(bodyparser.json());
 app.use(cors());
-
-db.on('error',console.error);
-db.once('open',()=>{
-    console.log("Connected to mongodb server");
-})
-//mongoose.connect(`mongodb://localhost/moviej_db`);
-mongoose.connect(`mongodb://heroku_lkcv7xs8:4hapnt3gst805fkqenk41ghics@ds251158.mlab.com:51158/heroku_lkcv7xs8`);
+const db=require('./db');
 const user=require('./models/users');
-const port=process.env.PORT || 8080;
+const port=process.env.PORT || 8000;
 const router=require('./routes')(app,user);
 const server=app.listen(port,()=>{
     console.log("Express server has started on port:: "+port);
