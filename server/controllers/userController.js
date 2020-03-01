bd70d1ae496701e6a75afc668f9b9abf103de1e0
leye195 <leye195@naver.com>
2020-03-01 22:36:47 +0900

    fix

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,5 @@
 import User from "../models/users";
 import Movie from "../models/movies";
-
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-dotenv.config();
-const jwtSecret = process.env.TOKEN_SECRET_KEY;
 import { generateToken, decodeToken } from "../token";
 //import Cookies from "universal-cookie";
 export const getUserList = async (req, res) => {
@@ -25,13 +20,6 @@ export const getUser = async (req, res) => {
     res.status(400).end();
   }
 };
-<<<<<<< HEAD
-/*export const getInfo = async (req, res) => {
-  const token = req.cookies.atk;
-  const info = jwt.verify(token, jwtSecret); //token decoding
-  res.json(info);
-};*/
-=======
 export const checkUser = async (req, res) => {
   console.log(req.headers);
   const token = req.headers["x-access-token"];
@@ -48,38 +36,29 @@ export const checkUser = async (req, res) => {
     return res.status(200).json({ token: refreshToken, user: token.user });
   }*/
 };
->>>>>>> test
 export const postSignUp = async (req, res) => {
   const { email, name, password } = req.body.params;
+  //console.log(email, name);
   try {
     const user = await User({ name, email });
     await User.register(user, password); //User Object,password
     res.status(200).json({ error: 0, result: 1 });
   } catch (error) {
+    //console.log(error);
     res.json({ error: 1, result: 0 });
   }
 };
 export const logout = async (req, res) => {
   req.logout();
-  res.cookie("access_token", null, {
-    maxAge: 0,
-    httpOnly: true
-  });
   res.status(200).json({ error: 0, result: 1 });
 };
 export const getLoginSuccess = async (req, res) => {
   const {
     params: { id }
   } = req;
-<<<<<<< HEAD
-
-  const token = jwt.sign({ uid: id }, jwtSecret, { expiresIn: "7d" });
-  res.status(200).json({ success: 1, msg: "success", uid: id, token: token });
-=======
   const token = generateToken({ id });
 
   res.status(200).json({ success: 1, msg: "success", uid: id, token });
->>>>>>> test
 };
 export const getLoginFailure = async (req, res) => {
   res.status(200).json({ success: 0, msg: "failure" });
